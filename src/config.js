e847fa346608954ded1071985d427ba8c4a5cfe3
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-25 16:45:37 -0500

    added 3

--- src/config.js
+++ src/config.js
@@ -1,22 +1,17 @@
 //? Dependencies
 require("dotenv").config();
 
-// const config = {
-//   port: process.env.PORT || 3000,
-//   nodeEnv: process.env.NODE_ENV || "development",
-//   jwtSecret: process.env.JWT_SECRET,
-//   host: process.env.HOST || "http://localhost:",
-// db: {
-//   host: process.env.DB_HOST || "127.0.0.1",
-//   username: process.env.DB_USER || "postgres",
-//   password: process.env.DB_PASS || "joseandres2012",
-//   dbName: process.env.DB_NAME,
-// },
-// };
-
-module.exports = {
-  port: process.env.PORT || 5000,
+const config = {
+  port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   jwtSecret: process.env.JWT_SECRET,
-  host: process.env.DOMAIN_HOST || "http://localhost:",
+  host: process.env.HOST || "http://localhost:3000",
+  db: {
+    host: process.env.DB_HOST || "localhost",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "root",
+    dbName: process.env.DB_NAME,
+  },
 };
+
+module.exports = config;
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
 const config = require("../config");
 
-/*
 const db = new Sequelize({
   dialect: "postgres",
   host: config.db.host, //? Variable de entorno del host
@@ -19,25 +18,5 @@ const db = new Sequelize({
         }
       : {},
 });
-*/
-module.exports = {
-  development: {
-    username: config.db.development.username,
-    password: config.db.development.password,
-    database: config.db.development.database,
-    host: "127.0.0.1",
-    dialect: config.db.development.dialect,
-  },
-  test: {
-    username: config.db.test.username,
-    password: config.db.test.password,
-    database: config.db.test.database,
-    host: "127.0.0.1",
-    dialect: config.db.test.dialect,
-  },
-  production: {
-    url: config.db.production.url,
-    use_env_variable: config.db.production.url,
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-  },
-};
+
+module.exports = db;
