677128d7c5e823b60c96fe2458d814cc1de6f1a2
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-25 16:17:07 -0500

    deploy

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const config = {
   port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   jwtSecret: process.env.JWT_SECRET,
-  host: process.env.HOST || "http://localhost:",
+  host: process.env.HOST || "0.0.0.0",
   db: {
-    host: process.env.DB_HOST || "http://localhost:",
+    host: process.env.DB_HOST || "0.0.0.0",
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASS || "root",
     dbName: process.env.DB_NAME,
