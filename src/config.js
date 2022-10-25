734c2965de5a1c12d5e705034c70cfcce6b9fac1
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-25 16:12:03 -0500

    deploy details

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@
 require("dotenv").config();
 
 const config = {
-  port: process.env.PORT || 9000,
+  port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   jwtSecret: process.env.JWT_SECRET,
-  host: process.env.HOST || "http://localhost:3000",
+  host: process.env.HOST || "http://localhost:",
   db: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "http://localhost:",
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASS || "root",
     dbName: process.env.DB_NAME,
